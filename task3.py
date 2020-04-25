edd4d05378c75c4a895ab512dd6313ee446ce52e
Ridwan Adeyemi <profread025@gmail.com>
2020-04-25 11:14:23 -0500

    Modified script file

--- task3.py
+++ task3.py
@@ -1,5 +1,7 @@
 import random
 
+userName = input("Enter a Username")
+
 def easyMode():
     print("\n___________________ EASY MODE ON___________________\n")
     easyModeNumber = random.randrange(1,10)
@@ -11,18 +13,20 @@ def easyMode():
             userInput = int(input(f"\n You have {guessCount} chances Guess the Number "))
             if (userInput == easyModeNumber):
                 print("\nYour guess is right")
+                print(f"YOU WON! {userName}")
                 print(f"You got the number right after {count} trial \n")
                 #break
                 menuOption()  
             elif (userInput < easyModeNumber):
-                print("\nThat was wrong\nYour guess is low")
+                print("\n Wrong guess, \n Your guess is low")
                 print(f"\nYou have {guessCount-count} chances left\n")
                 count += 1
                 continue
             else:
-                print("\nThat was wrong\nYour guess is high")
+                print("\n Wrong guess,\nYour guess is high")
                 print(f"\nYou have {guessCount-count} chances left\n")
                 count += 1
+
                 continue
         except ValueError:
             print("\n You Must Enter a Number")
@@ -41,16 +45,17 @@ def mediumMode():
             userInput = int(input("\nWhat is the number? "))
             if (userInput == mediumModeNumber):
                 print("\nYou guessed right")
+                print(f"YOU WON! {userName}")
                 print(f"You got it after {count} trials\n")
                 #break
                 menuOption()
             elif (userInput < mediumModeNumber):
-                print("\nThat was wrong\nYour guess is low")
+                print("\nTWrong guess,\nYour guess is low")
                 print(f"\nYou have {guessCount-count} guesses left\n")
                 count += 1
                 continue
             else:
-                print("\nThat was wrong\nYour guess is high")
+                print("\n Wrong guess,\nYour guess is high")
                 print(f"\nYou have {guessCount-count} guesses left\n")
                 count += 1
                 continue
@@ -70,16 +75,17 @@ def hardMode():
             userInput = int(input("\nWhat is the number? "))
             if (userInput == hardModeNumber):
                 print("\nYou guessed right")
+                print(f"YOU WON! {userName}")
                 print(f"You got it after {count} trials\n")
                 #break
                 menuOption()
             elif (userInput < hardModeNumber):
-                print("\nThat was wrong\nYour guess is low")
+                print("\n Wrong guess,\nYour guess is low")
                 print(f"\nYou have {guessCount-count} guesses left\n")
                 count += 1
                 continue
             else:
-                print("\nThat was wrong\nYour guess is high")
+                print("\n Wrong guess,\nYour guess is high")
                 print(f"\nYou have {guessCount-count} guesses left\n")
                 count += 1
                 continue
@@ -90,7 +96,8 @@ def hardMode():
     menuOption()
 
 def gameHomeMenu():
-    print("\n WELCOME, CAN YOU GUESS THE NUMBER? \n")
+    #print("\n WELCOME, CAN YOU GUESS THE NUMBER? \n")
+    print(f"\n WELCOME {userName}, CAN YOU GUESS THE NUMBER?")
     print("___________________ HOME___________________\n")
     print("1. Easy Mode")
     print("2. Medium Mode")
@@ -98,31 +105,52 @@ def gameHomeMenu():
     print("4. Exit the Game \n")
     print("Choose the level you wish to play \n")
 
-    promptQuestion = int(input("Reply: "))
-    if (promptQuestion == 1):
+    homeMenuPrompt = int(input("Reply: "))
+    if (homeMenuPrompt == 1):
         easyMode()
-    elif (promptQuestion == 2):
+    elif (homeMenuPrompt == 2):
         mediumMode()
-    elif (promptQuestion == 3):
+    elif (homeMenuPrompt == 3):
         hardMode()
-    elif (promptQuestion == 4):
+    elif (homeMenuPrompt == 4):
         exit()
     else:
         print("Enter a valid option")
         menuOption()
+
+
+"""def user_id (prompt):
+    if userName:
+            (print(f"\n Welecome {userName}, CAN YOU GUESS THE NUMBER?"))
+    while True:
+        try:
+             userName = str(input(prompt))
+        except ValueError:
+            print("Enter a Valid Username")
+        continue
+    if userName.isalpha():
+        print(f"\n WELCOME {userName}, CAN YOU GUESS THE NUMBER? \n")
+        continue
+        else:
+            print("Input a Valid UserName")
+        break
+    return userName """
+   
+
 #return the user to the manin menu or exit the game
 def menuOption():
     print("\nNice try, Do you want to try again? \n")
     print("\n-------------------------- KINDLY CHOOSE WHAT NEXT --------------------------\n")
-    print("1. Do you want to return to home page?")
-    print("2. Do you want to exit\n")
-    promptQuestion = input("Reply: ")
-    if (promptQuestion == '1'):
+    print("1. Return to home page?")
+    print("2. Exit the Game\n")
+    subhomeMenuPrompt = input("Reply: ")
+    if (subhomeMenuPrompt   == '1'):
         gameHomeMenu()
-    elif (promptQuestion == '2'):
+    elif (subhomeMenuPrompt  == '2'):
         exit()
     else:
         exit()
 var = 1
 while var == 1:  # it is used to create an infinite loop in order to continue running the program
     gameHomeMenu()
+
